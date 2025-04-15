dd0db57f7c631fee3d5d74604d2b2e352e9da3f2
Asif Ali <asifali27@gmail.com>
2025-04-16 01:27:27 +0530

    appointment api fix

---
+++ app/api/appointments/route.ts
@@ -0,0 +1,7 @@
+// app/api/appointments/route.ts
+
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({ message: 'Hello from appointments API' });
+}
